0419ebf61134a9688aa005cc68b879f638a81db9
lovit <soy.lovit@gmail.com>
2018-09-13 03:39:30 +0900

    Implemented score

--- crf_postagger/tagger.py
+++ crf_postagger/tagger.py
@@ -5,19 +5,32 @@ from .trainer import Feature
 class TrainedCRFTagger:
 
     def __init__(self, model_path=None, coefficients=None,
-        potential_function=None, verbose=False):
+        feature_transformer=None, verbose=False):
 
-        if potential_function is None:
-            potential_function = BaseFeatureTransformer()
+        if feature_transformer is None:
+            feature_transformer = BaseFeatureTransformer()
 
         self.coef = coefficients
-        self.potential_function = potential_function
+        self.feature_transformer = feature_transformer
         self.verbose = verbose
         if model_path:
             self._load_from_json(model_path)
 
-    def score(self, features):
-        raise NotImplemented
+    def score(self, sentence):
+
+        # feature transform
+        sentence_, tags = self.feature_transformer(sentence)
+
+        # transition weight
+        transitions = [(s0, s1) for s0, s1 in zip(tags, tags[1:])]
+        score = sum((self.transitions.get(trans, 0) for trans in transitions))
+
+        # state feature weight
+        for features, tag in zip(sentence_, tags):
+            for feature in features:
+                score += self.state_features.get((feature, tag), 0)
+
+        return score
 
     def tag(self, sentence):
         raise NotImplemented
