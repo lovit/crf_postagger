7e5c1ba8a99b7e6b6d90b5f2772264157dcbe96f
lovit <soy.lovit@gmail.com>
2018-09-13 03:02:01 +0900

    Implemented _load_from_json

--- crf_postagger/tagger.py
+++ crf_postagger/tagger.py
@@ -1,4 +1,6 @@
+import json
 from .transformer import potential_function as default_potential_function
+from .trainer import Feature
 
 class TrainedCRFTagger:
 
@@ -14,3 +16,30 @@ class TrainedCRFTagger:
 
     def tag(self, sentence):
         raise NotImplemented
+
+    def _load_from_json(self, json_path, marker = ' -> '):
+        with open(json_path, encoding='utf-8') as f:
+            model = json.load(f)
+
+        # parse transition
+        self.transitions = {
+            tuple(trans.split(marker)): coef
+            for trans, coef in model['transitions'].items()
+        }
+
+        # parse state features
+        self.state_features = {
+            tuple(feature.split(marker)): coef
+            for feature, coef in model['state_features'].items()
+        }
+
+        # get idx2features
+        self._idx2feature = model['idx2feature']
+
+        # parse feature information map
+        self._features = {
+            feature: Feature(idx, count)
+            for feature, (idx, count) in model['features'].items()
+        }
+
+        del model
