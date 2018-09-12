7e26ead471af632b1dcb8ca4fd19ec14499873af
lovit <soy.lovit@gmail.com>
2018-09-12 21:14:30 +0900

    Add utils as import path

--- crf_postagger/__init__.py
+++ crf_postagger/__init__.py
@@ -3,3 +3,5 @@ from .trainer import Trainer
 from .trainer import sent_to_xy
 from .transformer import potential_function
 from .utils import Corpus
+from .utils import check_dirs
+from .utils import get_process_memory
