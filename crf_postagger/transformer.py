3752a2443341ce3214e48f105d84180e9d9161c3
lovit <soy.lovit@gmail.com>
2018-09-13 03:31:11 +0900

    Feature transformer as class

--- crf_postagger/transformer.py
+++ crf_postagger/transformer.py
@@ -1,31 +1,49 @@
 bos = 'BOS'
 eos = 'EOS'
 
-def sentence_to_xy(sentence):
-    """Feature transformer
-    
-    :param list_of_tuple pos: a sentence [(word, tag), (word, tag), ...]
-    """
+class AbstractFeatureTransformer:
 
-    words, tags = zip(*sentence)
-    words_ = tuple((bos, *words, eos))
-    tags_ = tuple((bos, *tags, eos))
+    def __call__(self, sentence):
+        return self.sentence_to_xy(sentence)
 
-    encoded_sentence = potential_function(words_, tags_)
-    return encoded_sentence, tags
+    def sentence_to_xy(self, sentence):
+        raise NotImplemented
 
-def potential_function(words_, tags_):
-    n = len(tags_) - 2 # except bos & eos
-    sentence_ = [to_feature(words_, tags_, i) for i in range(1, n+1)]
-    return sentence_
+    def potential_function(self, sentence):
+        raise NotImplemented
 
-def to_feature(words_, tags_, i):
-    features = [
-        'x[0]=%s' % words_[i],
-        'x[0]=%s, y[-1]=%s' % (words_[i], tags_[i-1]),
-        'x[-1:0]=%s-%s' % (words_[i-1], words_[i]),
-        'x[-1:0]=%s-%s, y[-1]=%s' % (words_[i-1], words_[i], tags_[i-1]),
-        'x[-1,1]=%s-%s' % (words_[i-1], words_[i+1]),
-        'x[-1,1]=%s-%s, y[-1]=%s' % (words_[i-1], words_[i+1], tags_[i-1])
-    ]
-    return features
+    def to_feature(self, sentence):
+        raise NotImplemented
+
+class BaseFeatureTransformer(AbstractFeatureTransformer):
+    def __init__(self):
+        super().__init__()
+
+    def sentence_to_xy(self, sentence):
+        """Feature transformer
+
+        :param list_of_tuple pos: a sentence [(word, tag), (word, tag), ...]
+        """
+
+        words, tags = zip(*sentence)
+        words_ = tuple((bos, *words, eos))
+        tags_ = tuple((bos, *tags, eos))
+
+        encoded_sentence = self.potential_function(words_, tags_)
+        return encoded_sentence, tags
+
+    def potential_function(self, words_, tags_):
+        n = len(tags_) - 2 # except bos & eos
+        sentence_ = [self.to_feature(words_, tags_, i) for i in range(1, n+1)]
+        return sentence_
+
+    def to_feature(self, words_, tags_, i):
+        features = [
+            'x[0]=%s' % words_[i],
+            'x[0]=%s, y[-1]=%s' % (words_[i], tags_[i-1]),
+            'x[-1:0]=%s-%s' % (words_[i-1], words_[i]),
+            'x[-1:0]=%s-%s, y[-1]=%s' % (words_[i-1], words_[i], tags_[i-1]),
+            'x[-1,1]=%s-%s' % (words_[i-1], words_[i+1]),
+            'x[-1,1]=%s-%s, y[-1]=%s' % (words_[i-1], words_[i+1], tags_[i-1])
+        ]
+        return features
