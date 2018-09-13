1284b0f5e8c98b83eafefb1a96586f1886b719a6
lovit <soy.lovit@gmail.com>
2018-09-13 15:18:26 +0900

    Fix import path

--- crf_postagger/__init__.py
+++ crf_postagger/__init__.py
@@ -1,6 +1,7 @@
 from .tagger import TrainedCRFTagger
 from .trainer import Trainer
 from .transformer import BaseFeatureTransformer
+from .transformer import HMMStyleFeatureTransformer
 from .utils import Corpus
 from .utils import check_dirs
 from .utils import get_process_memory
