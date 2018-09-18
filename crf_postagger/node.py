c649fb27d5b92cc4a90dcc84550d5d2624270094
lovit <soy.lovit@gmail.com>
2018-09-19 04:02:08 +0900

    Simplify adding lemmas

--- crf_postagger/node.py
+++ crf_postagger/node.py
@@ -39,10 +39,22 @@ class AbstractNodeGenerator:
                 sub = eojeol[b:e]
                 for tag in self._get_pos(sub):
                     pos[b].append((sub, tag, tag, b+offset, e+offset))
+                for lemma_node in self._add_lemmas(sub, r, b, e, offset):
+                    pos[b].append(lemma_node)
         return pos
 
     def _get_pos(self, word):
         return [tag for tag, words in self.pos2words.items() if word in words]
+
+    def _add_lemmas(self, sub, r, b, e, offset):
+        for i in range(1, min(self.max_word_len, len(sub)) + 1):
+            try:
+                lemmas = self._lemmatize(sub, i)
+                for sub_, tag0, tag1 in lemmas:
+                    node = (sub_, tag0, tag1, b+offset, e+offset)
+                    yield node
+            except:
+                continue
 
     def _lemmatize(self, word, i):
         l = word[:i]
@@ -51,11 +63,11 @@ class AbstractNodeGenerator:
         len_word = len(word)
         for l_, r_ in lemma_candidate(l, r):
             word_ = l_ + ' + ' + r_
-            if (l_ in self.pos2words['Verb']) and (r_ in self.pos2words['Eomi']):
+            if (l_ in self.pos2words.get('Verb', {})) and (r_ in self.pos2words.get('Eomi', {})):
                 lemmas.append((word_, 'Verb', 'Eomi'))
-            if (l_ in self.pos2words['Adjective']) and (r_ in self.pos2words['Eomi']):
+            if (l_ in self.pos2words.get('Adjective', {})) and (r_ in self.pos2words.get('Eomi', {})):
                 lemmas.append((word_, 'Adjective', 'Eomi'))
-            if len_word > 1 and not (word in self.pos2words['Noun']):
+            if len_word > 1 and not (word in self.pos2words.get('Noun', {})):
                 if (l_ in self.pos2words['Noun']) and (r_ in self.pos2words['Josa']):
                     lemmas.append((word_, 'Noun', 'Josa'))
         return lemmas
