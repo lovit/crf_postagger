1226f6df3698fe07d5a2374530e597ce09ed8b07
lovit <soy.lovit@gmail.com>
2018-09-19 21:51:52 +0900

    Comment feature type

--- crf_postagger/transformer.py
+++ crf_postagger/transformer.py
@@ -61,11 +61,16 @@ class TrigramFeatureTransformer(AbstractFeatureTransformer):
 
     def to_feature(self, words_, tags_, i):
         features = [
+            # Capital: successive direction, lower case: previous
+            # word feature; X0
             'x[0]=%s' % words_[i],
+            # previous features; X0_y1, x10
             'x[0]=%s, y[-1]=%s' % (words_[i], tags_[i-1]),
             'x[-1:0]=%s-%s' % (words_[i-1], words_[i]),
+            # successive features; X01, X01_Y1
             'x[0:1]=%s-%s' % (words_[i], words_[i+1]),
             'x[0:1]=%s-%s, y[1]=%s' % (words_[i], words_[i+1], tags_[i+1]),
+            # both_side; X11, X101
             'x[-1,1]=%s-%s' % (words_[i-1], words_[i+1]),
             'x[-1:1]=%s-%s-%s' % (words_[i-1], words_[i], words_[i+1])
         ]
