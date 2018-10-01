b184f4c9fe039264c24bf07c4add525a59826a82
lovit <soy.lovit@gmail.com>
2018-10-02 00:21:30 +0900

    Clarify variable name word_l -> word_prev

--- crf_postagger/beam.py
+++ crf_postagger/beam.py
@@ -62,7 +62,7 @@ def _trigram_beam_search_cumulate_score(immature, pos, params, a_syllable_penalt
     noun_preference, longer_noun_preference):
 
     word, tag = pos[:2]
-    word_l, tag_l = immature.poses[-1][:2]
+    word_prev, tag_prev = immature.poses[-1][:2]
 
     score = immature.score
 
@@ -72,23 +72,23 @@ def _trigram_beam_search_cumulate_score(immature, pos, params, a_syllable_penalt
     score += longer_noun_preference * (len(word) - 1) if tag == 'Noun' else 0
 
     # transition score
-    score += params.transitions.get((tag_l, tag), 0)
+    score += params.transitions.get((tag_prev, tag), 0)
 
     # word feature
     score += params.pos2words.get(tag, {}).get(word, 0)
 
     # previous features
-    score += params.previous_1X0.get(tag, {}).get((word_l, word), 0)
-    score += params.previous_X0_1Y.get(tag, {}).get((word, tag_l), 0)
+    score += params.previous_1X0.get(tag, {}).get((word_prev, word), 0)
+    score += params.previous_X0_1Y.get(tag, {}).get((word, tag_prev), 0)
 
     # successive features (for previous pos)
-    score += params.successive_X01.get(tag_l, {}).get((word_l, word), 0)
-    score += params.successive_X01_Y1.get(tag_l, {}).get((word_l, word, tag), 0)
+    score += params.successive_X01.get(tag_prev, {}).get((word_prev, word), 0)
+    score += params.successive_X01_Y1.get(tag_prev, {}).get((word_prev, word, tag), 0)
 
     # bothside features (for previous pos)
     if len(immature.poses) >= 2:
-        word_ll, tag_ll = immature.poses[-2][:2]
-        score += params.bothside_1X1.get(tag_l, {}).get((word_ll, word), 0)
-        score += params.bothside_1X01.get(tag_l, {}).get((word_ll, word_l, word), 0)
+        word_prev2, tag_prev2 = immature.poses[-2][:2]
+        score += params.bothside_1X1.get(tag_prev, {}).get((word_prev2, word), 0)
+        score += params.bothside_1X01.get(tag_prev, {}).get((word_prev2, word_prev, word), 0)
 
     return score
