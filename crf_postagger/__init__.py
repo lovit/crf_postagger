7e0b57bb91c911e7cd13172ede7024eb94baa420
lovit <soy.lovit@gmail.com>
2018-10-05 00:22:57 +0900

    Fix import path

--- crf_postagger/__init__.py
+++ crf_postagger/__init__.py
@@ -2,6 +2,7 @@ from .lemmatizer import lemma_candidate
 from .params import AbstractParameter
 from .tagger import AbstractTagger
 from .trainer import Trainer
+from .transformer import AbstractFeatureTransformer
 from .transformer import BaseFeatureTransformer
 from .utils import Corpus
 from .utils import check_dirs
