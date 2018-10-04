e6c2eb0241dd3430de1bc57dbee3f21337a828ef
lovit <soy.lovit@gmail.com>
2018-10-04 23:54:48 +0900

    Merge hmm-style weight.py with tagger.py

--- crf_postagger/hmm_style/__init__.py
+++ crf_postagger/hmm_style/__init__.py
@@ -1,4 +1,3 @@
-from ._hmm_style import _hmm_style_tagger_weight
 from ._path import ford_list
 from ._tagger import HMMStyleParameter
 from ._tagger import HMMStyleTagger
--- crf_postagger/hmm_style/_tagger.py
+++ crf_postagger/hmm_style/_tagger.py
@@ -2,7 +2,6 @@ from .. import AbstractTagger
 from .. import AbstractParameter
 from .. import BaseFeatureTransformer
 from .. import bos, eos, unk, Eojeol, Eojeols
-from ._hmm_style import _hmm_style_tagger_weight
 from ._path import ford_list
 
 
@@ -49,6 +48,22 @@ class HMMStyleTagger(AbstractTagger):
             poses = self._remain_details(eojeols)
 
         return [poses, cost]
+
+def _hmm_style_tagger_weight(edges, parameters, _a_syllable_penalty, _noun_preference):
+    def get_transition(f, t):
+        return parameters.transitions.get((f, t), 0)
+
+    def get_score(from_, to_):
+        #score = get_transition(from_.last_tag, to_.first_tag) + to_.eojeol_score
+        score = get_transition(from_.last_tag, to_.first_tag) + from_.eojeol_score + to_.eojeol_score
+        if len(to_.first_word) == 1:
+            score += _a_syllable_penalty
+        elif to_.first_tag == 'Noun':
+            score += _noun_preference
+        #if not (to_.first_word == to_.last_tag):
+        #    score += get_transition(to_.first_tag, to_.last_tag)
+        return score
+    return [(from_, to_, get_score(from_, to_)) for from_, to_ in edges]
 
 class HMMStyleParameter(AbstractParameter):
     def __init__(self, model_path=None, pos2words=None, preanalyzed_eojeols=None,
