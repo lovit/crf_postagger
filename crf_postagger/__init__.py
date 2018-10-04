30ac3bff3c617eecd58dde5f191bc15f73daf106
lovit <soy.lovit@gmail.com>
2018-10-05 00:22:30 +0900

    Move all variables in common.py into utils.py

--- crf_postagger/__init__.py
+++ crf_postagger/__init__.py
@@ -1,13 +1,12 @@
-from .common import bos, eos, unk, Eojeol, BOS, Eojeols
 from .lemmatizer import lemma_candidate
 from .params import AbstractParameter
 from .tagger import AbstractTagger
 from .trainer import Trainer
-from .transformer import AbstractFeatureTransformer
 from .transformer import BaseFeatureTransformer
 from .utils import Corpus
 from .utils import check_dirs
 from .utils import get_process_memory
+from .utils import bos, eos, unk, Eojeol, BOS, Eojeols
 
 from . import hmm_style
 from . import trigram
